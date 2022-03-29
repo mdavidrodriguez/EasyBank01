6d69c18302c37af9286c14fa41b60fa43a8cc922
mdavidrodriguez <89699705+mdavidrodriguez@users.noreply.github.com>
2022-03-29 17:23:37 -0500

    Add files via upload

---
+++ EasyBank01/Entidad/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    public class Cliente
+    {
+        public string IdCliente { get; set; }
+        public  string Nombre { get; set; }
+        public Cliente(string idCliente, string nombre)
+        {
+            IdCliente = idCliente;
+
+            Nombre = nombre;
+            
+        }
+
+    }
+}
---
+++ EasyBank01/Entidad/Cuenta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    internal class Cuenta
+    {
+        public Cuenta(double numeroCuenta, Cliente cliente, double saldo)
+        {
+            NumeroCuenta = numeroCuenta;
+            this.cliente = cliente;
+            Saldo = saldo;
+        }
+
+        public double NumeroCuenta { get; set; }
+        public Cliente cliente { get; set; }
+        public double Saldo { get; set; }
+
+    }
+}
